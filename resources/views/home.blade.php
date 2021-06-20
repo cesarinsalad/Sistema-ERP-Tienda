12a01c05508671c33b5b594dca9f3dbebcc94552
Ali Rahi <alerahi1997@gmail.com>
2021-06-19 23:45:40 -0400

    guardado_3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,33 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'TRUCUPEY,C.A.')
+
+ <!-- @section('plugins.Sweetalert2', true) ventana emergente, te será muy util -->
+ 
+
+@section('content_header')
+    <h1>PANEL DE CONTROL</h1>
+@stop
 
 @section('content')
+    <p>Bienvenido</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> 
+  /*   Swal.fire(
+  'Good job!',
+  'You clicked the button!',
+  'success'
+) */
+    </script>
+@stop
+
+<!-- @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -19,5 +46,6 @@
             </div>
         </div>
     </div>
-</div>
+</div> 
 @endsection
+ -->
